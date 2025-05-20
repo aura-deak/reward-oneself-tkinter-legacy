9d2dcb0ac23600f2e74173169bb98c70505a789d
陈书涵 <2502820816@qq.com>
2025-05-20 00:55:23 +0000

    删除合并错误导致的多余代码
    Signed-off-by: 陈书涵 <2502820816@qq.com>

--- main.py
+++ main.py
@@ -209,15 +209,8 @@ def change_optional_features():
     if optionals is not None:
         if "启用AI" in optionals:
             enable_ai = True
-<<<<<<< HEAD
         else:
             enable_ai = False
-=======
-            print("启用AI")
-        else:
-            enable_ai = False
-            print("禁用AI")
->>>>>>> 6816cf3273b22acbe9503404a43ff52a11bc0a6f
     fresh_and_save()
 def ai():
     if enable_ai:
